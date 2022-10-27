c6b986eb629caf8a58bbc0f1a6dcc74ad33a3ee7
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-10-27 00:46:35 -0400

    reusable logic navbar footer components setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+
+import { Footer, Navbar } from '../components/index';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
 
+  <ThemeProvider attribute="class">
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/6e770e1687.js" crossorigin="anonymous" />
+  </ThemeProvider>
+
+);
 export default MyApp;
